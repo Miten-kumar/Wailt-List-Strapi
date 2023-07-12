09ac515017c150d7f8951c7a15a392dc1f8ca789
mitenAspire <mitenkumar.patel@aspiresoftserv.in>
2023-07-12 11:58:45 +0530

    "netlyfy"

--- config/plugins.js
+++ config/plugins.js
@@ -19,6 +19,20 @@ module.exports = ({ env }) => ({
   //     },
   //   },
   // },
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: "7Uc3MudU2PBAZoIItKASJi_DEwxDwQ1f6VaHMhhTGOQ",
+      sites: [
+        {
+          name: 'rococo-kashata-d6d511',
+          id: "6718d728-f599-49f5-a2e7-aa1b9ef9358a",
+          buildHook: "https://api.netlify.com/build_hooks/64ae3e73e16a4a0587101678",
+          branch: 'main' // optional
+        }
+      ]
+    },
+  },
   graphql: {
     enabled: true,
     config: {
